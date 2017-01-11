332d8acf24aaedaecf3e4af9d9cf4bbd48966ab4
Hemalatha <khema2010@gmail.com>
2017-01-10 20:12:27 -0800

    Feat: update media.py

--- media.py
+++ media.py
@@ -2,11 +2,11 @@ import webbrowser
 class Movie():
 	"""This program displys a list of favorite movies and its related information"""
 	
-	def __init__(self, movie_title, movie_story_line, poster_image_url, trailer_youtube_url):
+	def __init__(self, movie_title, movie_story_line, poster_image_url, trailer_youtube_link):
 		self.title = movie_title
 		self.story_line = movie_story_line
 		self.poster_image_url = poster_image_url
-		self.trailer_youtube_url = trailer_youtube_url
+		self.trailer_youtube_link = trailer_youtube_link
 
 
 	def show_trailer(self):
