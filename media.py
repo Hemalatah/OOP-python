a96f37c1869e0d6132ccc220e500456233231c63
Hemalatha <khema2010@gmail.com>
2017-01-22 21:15:43 -0800

    feat: added docstring

--- media.py
+++ media.py
@@ -1,8 +1,13 @@
 import webbrowser
 class Movie():
-	"""This program displys a list of favorite movies and its related information"""
+	""" This program displys a list of favorite movies and its related information """
 	
 	def __init__(self, movie_title, movie_story_line, poster_image_url, trailer_youtube_link):
+		""" This init function creates the object with the properties - 
+		movie_title,
+		movie_story_line,
+		poster_image_url,
+		trailer_yputube_link"""
 		self.title = movie_title
 		self.story_line = movie_story_line
 		self.poster_image_url = poster_image_url
@@ -10,4 +15,5 @@ class Movie():
 
 
 	def show_trailer(self):
+		""" This method opens the youtube link of the movie selected """
 		webbrowser.open(self.trailer_youtube_link)
